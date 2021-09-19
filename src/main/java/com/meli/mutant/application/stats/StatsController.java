ca0bb93dede4b483f02388b734a04fd2e6bdc02c
wilson.andrade <wilson.andrade@globant.com>
2021-09-18 19:39:14 -0500

    update swagger doc

--- src/main/java/com/meli/mutant/application/stats/StatsController.java
+++ src/main/java/com/meli/mutant/application/stats/StatsController.java
@@ -1,6 +1,11 @@
 package com.meli.mutant.application.stats;
 
 import com.meli.mutant.application.stats.domain.VerificationStats;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +18,10 @@ import reactor.core.publisher.Mono;
 public class StatsController {
     private final StatsService statsService;
 
+    @Operation(summary = " Obtener estadísticas de las verificaciones de ADN")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "ratio obtenido correctamente",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = VerificationStats.class)))
+    })
     @GetMapping("/verification")
     public Mono<VerificationStats> getVerificationStats() {
         return statsService.getVerificationStats();
